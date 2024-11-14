8701310235d8a65fe04d7851f4f486558718c56f
cjotasM <107132483+cjotasM@users.noreply.github.com>
2024-11-14 17:43:57 -0500

    Adding endpoint to do register in our app

--- Pet2Pet/app/src/main/java/com/example/frontpet2pet/data/models/request/RegisterRequest.java
+++ Pet2Pet/app/src/main/java/com/example/frontpet2pet/data/models/request/RegisterRequest.java
@@ -1,4 +1,61 @@
 package com.example.frontpet2pet.data.models.request;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RegisterRequest {
+    @SerializedName("user_name")
+    private String userName;
+
+    @SerializedName("user_last_name")
+    private String userLastName;
+
+    @SerializedName("user_email")
+    private String userEmail;
+
+    @SerializedName("password")
+    private String password;
+
+    @SerializedName("user_city")
+    private String userCity;
+
+    @SerializedName("user_country")
+    private String userCountry;
+
+    @SerializedName("user_number")
+    private String userNumber;
+
+    @SerializedName("user_bio")
+    private String userBio;
+
+    @SerializedName("role_id")
+    private Integer roleId;
+
+    // Constructor para los campos obligatorios
+    public RegisterRequest(String userName, String userLastName, String userEmail, String password) {
+        this.userName = userName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+
+    // Setters para campos opcionales
+    public void setUserCity(String userCity) {
+        this.userCity = userCity;
+    }
+
+    public void setUserCountry(String userCountry) {
+        this.userCountry = userCountry;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public void setUserBio(String userBio) {
+        this.userBio = userBio;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
 }
